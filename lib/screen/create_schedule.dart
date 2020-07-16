8964348ed1bd37e8c890053d6607dccb38ce0f02
masrobot <muhamadmashudiardiwinata@gmail.com>
2020-07-16 14:12:50 +0700

    add date picker

--- lib/screen/create_schedule.dart
+++ lib/screen/create_schedule.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:flutter/material.dart';
-import 'package:futsal_field_jepara/utils/constants.dart';
+import 'package:intl/intl.dart';
 
 final Firestore _fireStore = Firestore.instance;
 
@@ -10,6 +11,9 @@ class CreateSchedule extends StatefulWidget {
 }
 
 class _CreateScheduleState extends State<CreateSchedule> {
+  final format = DateFormat("dd-MM-yyyy");
+  String dateValue = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +21,45 @@ class _CreateScheduleState extends State<CreateSchedule> {
         title: Text("Buat Jadwal"),
         elevation: 0.0,
       ),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        color: kRedColor,
+      body: Padding(
+        padding: EdgeInsets.all(MediaQuery.of(context).size.width / 100 * 3),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            DateTimeField(
+                format: format,
+                onChanged: (value) {
+                  setState(() {
+                    dateValue = "${value.toString().split(' ')[0]}";
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: "Pilih Tanggal",
+                  labelStyle: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width / 100 * 3,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 2.0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 2.0,
+                    ),
+                  ),
+                ),
+                onShowPicker: (context, currentValue) {
+                  return showDatePicker(
+                    context: context,
+                    initialDate: currentValue ?? DateTime.now(),
+                    firstDate: DateTime(1900),
+                    lastDate: DateTime(2222),
+                  );
+                }),
+            Text(dateValue),
+          ],
+        ),
       ),
     );
   }
