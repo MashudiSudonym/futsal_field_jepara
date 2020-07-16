ae204ede3c8fcd04aed3f7053354dfba6d300930
masrobot <muhamadmashudiardiwinata@gmail.com>
2020-07-16 11:21:40 +0700

    Update edit_profile.dart

--- lib/screen/edit_profile.dart
+++ lib/screen/edit_profile.dart
@@ -1,17 +1,15 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:auto_route/auto_route.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:futsal_field_jepara/utils/constants.dart';
 import 'package:futsal_field_jepara/widget/text_field_custom.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:lottie/lottie.dart';
-import 'package:somedialog/somedialog.dart';
 
-final FirebaseAuth _auth = FirebaseAuth.instance;
 final Firestore _fireStore = Firestore.instance;
 final FirebaseStorage _fireStorage = FirebaseStorage.instance;
 
@@ -118,49 +116,46 @@ class _EditProfileState extends State<EditProfile> {
         ),
         body: Stack(
           children: <Widget>[
-            ListView(
-              children: <Widget>[
-                Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
+            (_isLoading && _typeOperation == TypeOperation.upload)
+                ? Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    color: Colors.white,
+                    child: Center(
+                      child: Lottie.asset(
+                        "assets/loading.json",
+                        height: MediaQuery.of(context).size.height / 100 * 25,
+                      ),
+                    ),
+                  )
+                : ListView(
                     children: <Widget>[
-                      _buildImageProfile(context),
-                      TextFieldCustom(
-                        title: "Full Name",
-                        hintText: "Full Name",
-                        textEditingController: _fullNameInputController,
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            _buildImageProfile(context),
+                            TextFieldCustom(
+                              title: "Full Name",
+                              hintText: "Full Name",
+                              textEditingController: _fullNameInputController,
+                            ),
+                            TextFieldCustom(
+                              title: "E-mail",
+                              hintText: "E-mail",
+                              textEditingController: _emailInputController,
+                            ),
+                            TextFieldCustom(
+                              title: "Address",
+                              hintText: "Address",
+                              textEditingController: _addressInputController,
+                            ),
+                            _buildPhoneNumberUser(context),
+                          ],
+                        ),
                       ),
-                      TextFieldCustom(
-                        title: "E-mail",
-                        hintText: "E-mail",
-                        textEditingController: _emailInputController,
-                      ),
-                      TextFieldCustom(
-                        title: "Address",
-                        hintText: "Address",
-                        textEditingController: _addressInputController,
-                      ),
-                      _buildPhoneNumberUser(context),
-                      (_isLoading && _typeOperation == TypeOperation.upload)
-                          ? Container(
-                              width: double.infinity,
-                              height: double.infinity,
-                              color: Colors.white,
-                              child: Center(
-                                child: Lottie.asset(
-                                  "assets/loading.json",
-                                  height: MediaQuery.of(context).size.height /
-                                      100 *
-                                      25,
-                                ),
-                              ),
-                            )
-                          : Container(),
                     ],
                   ),
-                ),
-              ],
-            ),
           ],
         ),
       ),
@@ -351,123 +346,75 @@ class _EditProfileState extends State<EditProfile> {
   }
 
   _uploadData(BuildContext context) async {
-    if (_image == null) {
-      return SomeDialog(
-        context: context,
-        path: "assets/report.json",
-        title: "Peringatan",
-        content: "Anda belum mengambil gambar profil.",
-        submit: () {},
-        mode: SomeMode.Lottie,
-        appName: "Futsal Field Jepara",
-        buttonConfig: ButtonConfig(
-          buttonDoneColor: kLogoLightGreenColor,
-          dialogDone: "Oke",
-          dialogCancel: "",
-          buttonCancelColor: kPrimaryColor,
-        ),
-      );
-    } else if (_fullNameInputController.text.isEmpty) {
-      return SomeDialog(
-        context: context,
-        path: "assets/report.json",
-        title: "Peringatan",
-        content: "Anda belum mengisi nama lengkap.",
-        submit: () {},
-        mode: SomeMode.Lottie,
-        appName: "Futsal Field Jepara",
-        buttonConfig: ButtonConfig(
-          buttonDoneColor: kLogoLightGreenColor,
-          dialogDone: "Oke",
-          dialogCancel: "",
-          buttonCancelColor: kPrimaryColor,
-        ),
-      );
-    } else if (_emailInputController.text.isEmpty) {
-      return SomeDialog(
-        context: context,
-        path: "assets/report.json",
-        title: "Peringatan",
-        content: "Anda belum mengisi alamat email.",
-        submit: () {},
-        mode: SomeMode.Lottie,
-        appName: "Futsal Field Jepara",
-        buttonConfig: ButtonConfig(
-          buttonDoneColor: kLogoLightGreenColor,
-          dialogDone: "Oke",
-          dialogCancel: "",
-          buttonCancelColor: kPrimaryColor,
-        ),
-      );
-    } else if (_addressInputController.text.isEmpty) {
-      return SomeDialog(
-        context: context,
-        path: "assets/report.json",
-        title: "Peringatan",
-        content: "Anda belum mengisi alamat lengkap.",
-        submit: () {},
-        mode: SomeMode.Lottie,
-        appName: "Futsal Field Jepara",
-        buttonConfig: ButtonConfig(
-          buttonDoneColor: kLogoLightGreenColor,
-          dialogDone: "Oke",
-          dialogCancel: "",
-          buttonCancelColor: kPrimaryColor,
-        ),
-      );
-    }
-
     StorageReference _ref = _fireStorage
         .ref()
-        .child("user-profile-$widget.uid")
-        .child("user-photo-$widget.uid");
-    StorageUploadTask _uploadTask = _ref.putFile(_image);
-    StreamSubscription _streamSubscription =
-        _uploadTask.events.listen((event) async {
-      var eventType = event.type;
-      if (eventType == StorageTaskEventType.progress) {
-        setState(() {
-          _typeOperation = TypeOperation.upload;
-          _isLoading = true;
-        });
-      } else if (eventType == StorageTaskEventType.failure) {
-        Scaffold.of(context).showSnackBar(
-          SnackBar(
-            content: Text("Photo failed to upload"),
-          ),
-        );
+        .child("user-profile-${widget.uid}")
+        .child("user-photo-${widget.uid}");
 
-        setState(() {
-          _isLoading = false;
-          _isSuccess = false;
-          _typeOperation = null;
-        });
-      } else if (eventType == StorageTaskEventType.success) {
-        try {
-          var downloadUrl = await event.snapshot.ref.getDownloadURL();
-          var userData = await _fireStore
-              .collection("users")
-              .document(widget.uid)
-              .setData({
-            'uid': widget.uid,
-            'name': _fullNameInputController.text,
-            'email': _emailInputController.text,
-            'phone': _userPhone,
-            'address': _addressInputController.text,
-            'imageProfile': downloadUrl.toString(),
+    if (_image != null) {
+      StorageUploadTask _uploadTask = _ref.putFile(_image);
+      StreamSubscription _streamSubscription =
+          _uploadTask.events.listen((event) async {
+        var eventType = event.type;
+        if (eventType == StorageTaskEventType.progress) {
+          setState(() {
+            _typeOperation = TypeOperation.upload;
+            _isLoading = true;
           });
-
+        } else if (eventType == StorageTaskEventType.failure) {
           setState(() {
             _isLoading = false;
-            _isSuccess = true;
+            _isSuccess = false;
             _typeOperation = null;
           });
-        } catch (e) {
-          print(e);
+        } else if (eventType == StorageTaskEventType.success) {
+          try {
+            var downloadUrl = await event.snapshot.ref.getDownloadURL();
+            var userData = await _fireStore
+                .collection("users")
+                .document(widget.uid)
+                .updateData({
+              'uid': widget.uid,
+              'name': _fullNameInputController.text,
+              'email': _emailInputController.text,
+              'phone': _userPhone,
+              'address': _addressInputController.text,
+              'imageProfile': downloadUrl.toString(),
+            });
+
+            ExtendedNavigator.of(context).pop();
+
+            setState(() {
+              _isLoading = false;
+              _isSuccess = true;
+              _typeOperation = null;
+            });
+          } catch (e) {
+            print(e);
+          }
         }
+      });
+      await _uploadTask.onComplete;
+      _streamSubscription.cancel();
+    } else {
+      try {
+        var downloadUrl = await _ref.getDownloadURL();
+        var userData = await _fireStore
+            .collection("users")
+            .document(widget.uid)
+            .updateData({
+          'uid': widget.uid,
+          'name': _fullNameInputController.text,
+          'email': _emailInputController.text,
+          'phone': _userPhone,
+          'address': _addressInputController.text,
+          'imageProfile': downloadUrl.toString(),
+        });
+
+        ExtendedNavigator.of(context).pop();
+      } catch (e) {
+        print(e);
       }
-    });
-    await _uploadTask.onComplete;
-    _streamSubscription.cancel();
+    }
   }
 }
