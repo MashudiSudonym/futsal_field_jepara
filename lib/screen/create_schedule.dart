b4a407dee8f49a8f4d11ce1ff3f1e5bcd5a8b36f
masrobot <muhamadmashudiardiwinata@gmail.com>
2020-07-18 13:12:31 +0700

    update layout create schedule

--- lib/screen/create_schedule.dart
+++ lib/screen/create_schedule.dart
@@ -13,6 +13,58 @@ class _CreateScheduleState extends State<CreateSchedule> {
   TextEditingController _datePickerController = TextEditingController();
   String _dateValue = "";
   DateTime _date = DateTime.now();
+  List<String> _fieldTypeList = [
+    "Lapangan Flooring",
+    "Lapangan Sintetis",
+  ];
+  List<String> _timeOrderList = [
+    "09.00",
+    "10.00",
+    "12.00",
+    "13.00",
+    "14.00",
+    "15.00",
+    "16.00",
+    "17.00",
+    "18.00",
+    "19.00",
+    "20.00",
+    "21.00",
+    "22.00",
+    "23.00",
+  ];
+  int _fieldTypeSelectedIndex = 0;
+  int _timeOrderStartSelectedIndex = 0;
+  int _timeOrderFinishSelectedIndex = 0;
+
+  @override
+  void initState() {
+    _onFieldTypeSelected(0);
+    _onTimeOrderStartSelected(0);
+    _onTimeOrderFinishSelected(0);
+    super.initState();
+  }
+
+  void _onFieldTypeSelected(int index) {
+    setState(() {
+      _fieldTypeSelectedIndex = index;
+      print("${_fieldTypeList[index]} $_fieldTypeSelectedIndex");
+    });
+  }
+
+  void _onTimeOrderStartSelected(int index) {
+    setState(() {
+      _timeOrderStartSelectedIndex = index;
+      print("${_timeOrderList[index]} $_timeOrderStartSelectedIndex");
+    });
+  }
+
+  void _onTimeOrderFinishSelected(int index) {
+    setState(() {
+      _timeOrderFinishSelectedIndex = index;
+      print("${_timeOrderList[index]} $_timeOrderFinishSelectedIndex");
+    });
+  }
 
   @override
   void dispose() {
@@ -48,7 +100,7 @@ class _CreateScheduleState extends State<CreateSchedule> {
           title: Text("Buat Jadwal"),
           elevation: 0.0,
         ),
-        body: Padding(
+        body: SingleChildScrollView(
           padding: EdgeInsets.all(MediaQuery.of(context).size.width / 100 * 4),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -70,17 +122,179 @@ class _CreateScheduleState extends State<CreateSchedule> {
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(
-                      color: kLogoLightGreenColor,
+                      color: Colors.greenAccent[400],
                     ),
                   ),
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(
-                      color: kLogoLightGreenColor,
+                      color: Colors.greenAccent[400],
                     ),
                   ),
                 ),
               ),
-              Text(_datePickerController.text),
+              SizedBox(height: MediaQuery.of(context).size.height / 100 * 2),
+              Text(
+                "Jenis Lapangan",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height / 100 * 1),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 100 * 8,
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 3,
+                    crossAxisSpacing:
+                        MediaQuery.of(context).size.width / 100 * 2,
+                    mainAxisSpacing: 2,
+                  ),
+                  itemCount: _fieldTypeList.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: (_fieldTypeSelectedIndex != null &&
+                              _fieldTypeSelectedIndex == index)
+                          ? Colors.greenAccent[400]
+                          : kPrimaryColor,
+                      child: ListTile(
+                        title: Text(
+                          _fieldTypeList[index],
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: (_fieldTypeSelectedIndex != null &&
+                                    _fieldTypeSelectedIndex == index)
+                                ? kPrimaryColor
+                                : Colors.black,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        onTap: () {
+                          setState(() {
+                            _onFieldTypeSelected(index);
+                          });
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height / 100 * 2),
+              Text(
+                "Waktu Mulai",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height / 100 * 1),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 100 * 28,
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    childAspectRatio: 16 / 9,
+                    mainAxisSpacing: 2,
+                  ),
+                  itemCount: _timeOrderList.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: (_timeOrderStartSelectedIndex != null &&
+                              _timeOrderStartSelectedIndex == index)
+                          ? Colors.greenAccent[400]
+                          : kPrimaryColor,
+                      child: Center(
+                        child: ListTile(
+                          title: Text(
+                            _timeOrderList[index],
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: (_timeOrderStartSelectedIndex != null &&
+                                      _timeOrderStartSelectedIndex == index)
+                                  ? kPrimaryColor
+                                  : Colors.black,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          onTap: () {
+                            setState(() {
+                              _onTimeOrderStartSelected(index);
+                            });
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height / 100 * 2),
+              Text(
+                "Waktu Selesai",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height / 100 * 1),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 100 * 28,
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    childAspectRatio: 16 / 9,
+                    mainAxisSpacing: 2,
+                  ),
+                  itemCount: _timeOrderList.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: (_timeOrderFinishSelectedIndex != null &&
+                              _timeOrderFinishSelectedIndex == index)
+                          ? Colors.greenAccent[400]
+                          : kPrimaryColor,
+                      child: Center(
+                        child: ListTile(
+                          title: Text(
+                            _timeOrderList[index],
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: (_timeOrderFinishSelectedIndex != null &&
+                                      _timeOrderFinishSelectedIndex == index)
+                                  ? kPrimaryColor
+                                  : Colors.black,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          onTap: () {
+                            setState(() {
+                              _onTimeOrderFinishSelected(index);
+                            });
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height / 100 * 5),
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height / 100 * 7,
+                padding: EdgeInsets.all(
+                    MediaQuery.of(context).size.height / 100 * 1),
+                child: RaisedButton(
+                  child: Text(
+                    "Pesan Lapangan",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  color: Colors.greenAccent[400],
+                  onPressed: () {
+                    print("Buat pesanan");
+                  },
+                ),
+              ),
             ],
           ),
         ),
